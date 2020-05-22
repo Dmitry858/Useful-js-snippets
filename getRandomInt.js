e16f797913705224f2a0def163fdfa40b26c27f5
Dmitry858 <39441805+Dmitry858@users.noreply.github.com>
2020-05-22 12:28:03 +0400

    Edit getRandomInt.js

--- getRandomInt.js
+++ getRandomInt.js
@@ -1,3 +1,7 @@
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+// Получение случайного элемента из массива
+let items = [12, 548, 'a', 2, 5478, 'foo', 8852, 'Doe', 2145, 119];
+let randomItem = items[Math.floor(Math.random() * items.length)];
